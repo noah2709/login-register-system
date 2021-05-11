0aff93c44275269401b31953d8de8e420e19f62b
EliaPlate <eliaplate2002@googlemail.com>
2021-05-11 10:46:28 +0200

    Updated Backend
    Updated Backend

--- inc/functions.inc.php
+++ inc/functions.inc.php
@@ -1,47 +1,48 @@
 <?php
 
-function emptyInputSignup($username, $email, $pwd, $pwdRepeat) {
+function emptyInputSignup($username, $email, $pwd, $pwdRepeat)
+{
     if (empty($username) || empty($email) || empty($pwd) || empty($pwdRepeat)) {
         $result = true;
-    }
-    else {
+    } else {
         $result = false;
     }
-    return $result;   
+    return $result;
 }
 
-function invalidUsername($username) {
+function invalidUsername($username)
+{
     if (!preg_match("/^[a-zA-Z0-9]*$/", $username)) {
         $result = true;
-    } 
-    else {
+    } else {
         $result = false;
     }
-    return $result;   
+    return $result;
 }
 
-function invalidEmail($email) {
+function invalidEmail($email)
+{
     if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
         $result = true;
-    } 
-    else {
+    } else {
         $result = false;
     }
-    return $result;   
+    return $result;
 }
 
-function pwdMatch($pwd, $pwdRepeat) {
+function pwdMatch($pwd, $pwdRepeat)
+{
     if ($pwd !== $pwdRepeat) {
         $result = true;
-    } 
-    else {
+    } else {
         $result = false;
     }
-    return $result;   
+    return $result;
 }
 
-function UsernameExists($conn, $username, $email) {
-    $sql    = "SELECT * FROM users WHERE userName = ? OR userEmail = ?;";
+function UsernameExists($conn, $username, $email)
+{
+    $sql    = "SELECT * FROM user WHERE username = ? OR email = ?;";
     $stmt   = mysqli_stmt_init($conn);
     if (!mysqli_stmt_prepare($stmt, $sql)) {
         header("location: ../signup.php?error=SELECTFAILED");
@@ -55,41 +56,69 @@ function UsernameExists($conn, $username, $email) {
 
     if ($row = mysqli_fetch_assoc($resultData)) {
         return $row;
+    } else {
+        $result = false;
+        return $result;
     }
-    else {
+}
+function HumanNameExists($conn, $firstname, $lastname)
+{
+    $sql    = "SELECT * FROM user WHERE firstname = ? OR lastname = ?;";
+    $stmt   = mysqli_stmt_init($conn);
+    if (!mysqli_stmt_prepare($stmt, $sql)) {
+        header("location: ../signup.php?error=SELECTFAILED");
+        exit();
+    }
+
+    mysqli_stmt_bind_param($stmt, "ss", $firstname, $lastname);
+    mysqli_stmt_execute($stmt);
+
+    $resultData = mysqli_stmt_get_result($stmt);
+
+    if ($row = mysqli_fetch_assoc($resultData)) {
+        return $row;
+    } else {
         $result = false;
         return $result;
     }
 }
 
-function createUser($conn, $username, $email, $pwd) {
-    $sql    = "INSERT INTO users (userName, userEmail, userPwd) VALUES (?, ?, ?);";
+function createUser($conn, $firstname, $lastname, $username, $password, $email)
+// 
+// Role_id 0 = Admin
+// Role_id 1 = User
+// Role_id 2 = Trainer
+// 
+// 
+{
+    $sql    = "INSERT INTO user (firstname, lastname, username, password, email, role_id) VALUES (?, ?, ?, ?, ?, ?);";
     $stmt   = mysqli_stmt_init($conn);
     if (!mysqli_stmt_prepare($stmt, $sql)) {
         header("location: ../signup.php?error=INSERTFAILED");
         exit();
     }
 
-    $hashedPwd = password_hash($pwd, PASSWORD_DEFAULT);
+    $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
 
-    mysqli_stmt_bind_param($stmt, "sss", $username, $email, $hashedPwd);
+    mysqli_stmt_bind_param($stmt, "sssssi", $firstname, $lastname, $username, $hashedPassword, $email, 1);
     mysqli_stmt_execute($stmt);
     mysqli_stmt_close($stmt);
     header("location: ../signup.php?error=none");
-        exit();
+    exit();
 }
 
-function emptyInputLogin($username, $pwd) {
+function emptyInputLogin($username, $pwd)
+{
     if (empty($username) || empty($pwd)) {
         $result = true;
-    }
-    else {
+    } else {
         $result = false;
     }
-    return $result;   
+    return $result;
 }
 
-function loginUser($conn, $username, $pwd) {
+function loginUser($conn, $username, $pwd)
+{
     $usernameExists = UsernameExists($conn, $username, $username);
 
     if ($usernameExists === false) {
@@ -103,8 +132,7 @@ function loginUser($conn, $username, $pwd) {
     if ($checkPwd === false) {
         header("location: ../login.php?error=wronglogin2");
         exit();
-    }
-    else if ($checkPwd === true) {
+    } else if ($checkPwd === true) {
         session_start();
         $_SESSION['userid']     = $usernameExists['userId'];
         $_SESSION['username']   = $usernameExists['userName'];
--- inc/signup.inc.php
+++ inc/signup.inc.php
@@ -1,17 +1,19 @@
 <?php
 
 if (isset($_POST["submit"])) {
-    
+
     require_once 'db.inc.php';
 
     $username   = mysqli_real_escape_string($conn, $_POST["username"]);
     $email      = mysqli_real_escape_string($conn, $_POST["email"]);
     $pwd        = mysqli_real_escape_string($conn, $_POST["pwd"]);
     $pwdRepeat  = mysqli_real_escape_string($conn, $_POST["pwdRepeat"]);
+    $firstname  = mysqli_real_escape_string($conn, $_POST["firstname"]);
+    $lastname  = mysqli_real_escape_string($conn, $_POST["lastname"]);
 
     require_once 'functions.inc.php';
 
-    if (emptyInputSignup($username, $email, $pwd, $pwdRepeat) !== false) {
+    if (emptyInputSignup($username, $email, $pwd, $pwdRepeat, $firstname, $lastname) !== false) {
         header("location: ../signup.php?error=emptyinput");
         exit();
     }
@@ -31,11 +33,13 @@ if (isset($_POST["submit"])) {
         header("location: ../signup.php?error=usernametaken");
         exit();
     }
+    if (HumanNameExists($conn, $firstname, $lastname) !== false) {
+        header("location: ../signup.php?error=humanexist");
+        exit();
+    }
 
-    createUser($conn, $username, $email, $pwd);
-
-}
-else {
+    createUser($conn, $firstname, $lastname, $username, $pwd, $email);
+} else {
     header("location: ../signup.php");
     exit();
 }
