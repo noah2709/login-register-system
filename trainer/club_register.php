213a5ace31f2aa2c200873ad26efce7740002473
EliaPlate <eliaplate2002@googlemail.com>
2021-05-20 20:30:35 +0200

    Added Permission
    Added Permission

--- trainer/club_register.php
+++ trainer/club_register.php
@@ -1,6 +1,15 @@
 <?php
-
+session_start();
 require_once '../inc/db.inc.php';
+require_once '../inc/functions.inc.php';
+
+if (isset($_SESSION)) {
+    if (!isTrainer($conn, $_SESSION['userid']) and !isAdmin($conn, $_SESSION['userid'])) {
+        header("location: ../error/error_403_page.php");
+    }
+} else {
+    header("location: ../error/error_403_page.php");
+}
 
 ?>
 
