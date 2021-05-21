5d5e50d20497343e6c36801de37841ea0c39377e
EliaPlate <eliaplate2002@googlemail.com>
2021-05-21 13:59:05 +0200

    Added Socialfly
    Added Socialfly

--- calendar/calendar.php
+++ calendar/calendar.php
@@ -8,11 +8,13 @@ session_start();
 <head>
     <title>Alle Termine</title>
     <link rel="stylesheet" href="../fullcalender.css" />
+    <link rel="stylesheet" href="../style.css" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha.6/css/bootstrap.css" />
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.18.1/moment.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.4.0/fullcalendar.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
     <script src="//cdn.jsdelivr.net/npm/sweetalert2@10"></script>
     <script>
         $(document).ready(function() {
@@ -57,6 +59,14 @@ session_start();
 </head>
 
 <body>
+    <div class="social_flyout">
+        <ul class="some_list">
+            <li><a href="../../index.php"><i class="fas fa-backward"></i></a></li>
+            <li><a href="https://github.com/Taikador/login-register-system"><i class="fab fa-github"></i></a></li>
+            <li><a href="#"><i class="fab fa-youtube"></i></a></li>
+            <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+        </ul>
+    </div>
     <br />
     <h2 align="center">Alle Termine</h2>
     <br />
