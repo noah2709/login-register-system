a018debda3fc39193acc81194642f85c9e27d662
EliaPlate <eliaplate2002@googlemail.com>
2021-05-23 14:51:57 +0200

    Calendar for club next events

--- trainer/club_events.php
+++ trainer/club_events.php
@@ -1,31 +1,62 @@
 <?php
 session_start();
-include_once '../inc/db.inc.php';
-include_once '../inc/functions.inc.php';
 ?>
 
-<html lang="en">
+<!DOCTYPE html>
+<html>
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../style.css">
-    <script src="https://kit.fontawesome.com/2deba413ff.js" crossorigin="anonymous"></script>
-    <link href="https://fonts.googleapis.com/css2?famsily=Montserrat:wght@200&display=swap" rel="stylesheet">
-    <title>Ihre nächsten Spiele</title>
+    <title>Ihre Wettkämpfe</title>
+    <link rel="stylesheet" href="../fullcalender.css" />
+    <link rel="stylesheet" href="../style.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha.6/css/bootstrap.css" />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.18.1/moment.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.4.0/fullcalendar.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
+    <script src="//cdn.jsdelivr.net/npm/sweetalert2@10"></script>
+    <script>
+        $(document).ready(function() {
+            var calendar = $('#calendar').fullCalendar({
+                eventStartEditable: false,
+                editable: true,
+                header: {
+                    left: 'prev,next today',
+                    center: 'title',
+                    right: 'month,agendaWeek,agendaDay'
+                },
+                events: 'club_events_load.php',
+                selectable: true,
+                selectHelper: true,
+                eventClick: function(event) {
+                    var start = (event.start == null ? "Unbekannt" : $.fullCalendar.formatDate(event.start, "Y-MM-DD HH:mm:ss"));
+                    var end = (event.end == null ? "Unbekannt" : $.fullCalendar.formatDate(event.end, "Y-MM-DD HH:mm:ss"));
+                    var title = event.title;
+
+                    const formData = new FormData();
+                    fetch('', {
+                        method: "POST",
+                        body: formData
+                    }).then(function(response) {}).then(function(text) {
+                        console.log(text);
+                    }).catch(function(error) {
+                        concole.error(error);
+                    }).then(function(onclick) {
+                        Swal.fire({
+                            position: 'center',
+                            titleText: 'Informationen:',
+                            html: "Start: " + start + "<br> Ende: " + end + "<br> Titel: " + title,
+                            showConfirmButton: true,
+                        })
+                    })
+                },
+            });
+        });
+    </script>
 </head>
 
 <body>
-    <?php
-    $userid = $_SESSION['userid'];
-    $club = getClub($conn, $userid);
-    $clubName = $club['name'];
-    echo "<div class='club_events_h1'>";
-    echo "<h1>Ihre nächsten Spiele im Club: $clubName</h1>";
-    echo "</div>";
-    ?>
-
     <div class="social_flyout">
         <ul class="some_list">
             <li><a href="../index.php"><i class="fas fa-backward"></i></a></li>
@@ -34,68 +65,37 @@ include_once '../inc/functions.inc.php';
             <li><a href="#"><i class="fab fa-twitter"></i></a></li>
         </ul>
     </div>
-    <div class="wrapper">
-        <table class="club_events_content">
-            <thead>
-                <tr>
-                    <th>Start</th>
-                    <th>Ende</th>
-                    <th>Winner</th>
-                    <th>Club 1</th>
-                    <th>Club 2</th>
-                    <th>Golfplatz</th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php
-                $userid = $_SESSION['userid'];
-                $club = getClub($conn, $userid);
-                $clubId = $club['club_id'];
-
-                $query = $conn->query("SELECT * FROM event WHERE club_id1 = $clubId OR club_id2 = $clubId");
-                if ($query->num_rows <= 0) return;
-
-
-
-                while ($row = $query->fetch_assoc()) {
-                    $currentFullDate = date("Y-m-d H:i:s");
-                    $dataBaseFullDate = $row['starttime'];
-
-                    $dataBaseDate = explode(" ", $dataBaseFullDate)[0];
-                    $currentDate = date("Y-m-d");
-
-                    if ($dataBaseFullDate < $currentFullDate) {
-                        continue;
-                    }
-                    if ($dataBaseDate == $currentDate) {
-                ?>
-
-                        <tr>
-                            <td style="background: lime;"><?php echo $row['starttime'] ?>--</td>
-                            <td style="background: lime;"><?php echo $row['endtime'] ?></td>
-                            <td style="background: lime;"><?php echo ($row['winner'] == -1) ? "Unbekannt" : $row['winner']; ?></td>
-                            <td style="background: lime;"><?php echo getClubFromClubId($conn, $row['club_id1'])['name'] ?></td>
-                            <td style="background: lime;"><?php echo getClubFromClubId($conn, $row['club_id2'])['name'] ?></td>
-                            <td style="background: lime;"><?php echo getCourtFromId($conn, $row['court_id']) ?></td>
-                        </tr>
-                    <?php
-                        continue;
-                    }
-                    ?>
-
-                    <tr>
-                        <td><?php echo $row['starttime'] ?>--</td>
-                        <td><?php echo $row['endtime'] ?></td>
-                        <td><?php echo ($row['winner'] == -1) ? "Unbekannt" : $row['winner']; ?></td>
-                        <td><?php echo getClubFromClubId($conn, $row['club_id1'])['name'] ?></td>
-                        <td><?php echo getClubFromClubId($conn, $row['club_id2'])['name'] ?></td>
-                        <td><?php echo getCourtFromId($conn, $row['court_id']) ?></td>
-
-                    </tr>
-                <?php
-                }
-                ?>
-            </tbody>
-        </table>
+    <br />
+    <br />
+    <div class="container">
+        <div id="calendar"></div>
     </div>
+    <div class='centered_pictures'>
+        <div class='slidershow middle'>
+            <div class='slides'>
+                <input type='radio' name='r' id='r1' checked hidden>
+                <input type='radio' name='r' id='r2' hidden>
+                <input type='radio' name='r' id='r3' hidden>
+                <div class='slide s1'>
+                    <img style='opacity: 0.76;' src='../img/golf_4.jpg'> alt="picture 1 not found">
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="datenschutz__impressum">
+        <ul>
+            <li>
+                <a href="../impressum.php">Impressum</a>
+            </li>
+            <li>
+                <a href="../privacy.php">Datenschutz</a>
+            </li>
+        </ul>
+    </div>
+
+
+    <footer><i class="far fa-copyright"> Copyright 2021</i></footer>
 </body>
+
+</html>
