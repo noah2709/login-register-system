bfbcf6ad7618721897b2dac7e00dc8a3aeeb7999
EliaPlate <eliaplate2002@googlemail.com>
2021-05-23 14:50:26 +0200

    Added Impressum

---
+++ impressum.php
@@ -0,0 +1,75 @@
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <script src="https://kit.fontawesome.com/2deba413ff.js" crossorigin="anonymous"></script>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@200&display=swap" rel="stylesheet">
+    <title>Datenschutz</title>
+</head>
+
+<body class="privacy__body">
+
+    <div class="privacy__title__container">
+        <div class="privay__title">
+            <h1>Impressum</h1>
+        </div>
+    </div>
+    <div class="social_flyout">
+        <ul class="some_list">
+            <li><a href="index.php"><i class="fas fa-backward"></i></a></li>
+            <li><a href="https://github.com/Taikador/login-register-system"><i class="fab fa-github"></i></a></li>
+            <li><a href="#"><i class="fab fa-youtube"></i></a></li>
+            <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+        </ul>
+    </div>
+
+    <div class="impressum__content__container">
+        <h2 class="entry__content h2">Rechtliche Hinweise</h2>
+
+        <h3 class="entry__content h3">Angaben gemäß § 5 TMG</h3>
+
+        <p class="small_margin_bottom">Golfverband-Niedersachsen</p>
+        <p class="small_margin_bottom">Widukindweg</p>
+        <p class="small_margin_bottom">21357 Barum</p>
+
+        <p class="high_margin_top" id="small_margin_bottom">Vereinsregister: HRB 999999</p>
+        <p class="small_margin_bottom">Registergericht: Amtsgericht Hodenhagen</p>
+
+        <p class="high_margin_top" id="small_margin_bottom" style="font-weight: bold;">Vertreten durch:</p>
+        <p>Dr. Andi Mauer</p>
+
+        <h3 class="entry__content h3">Kontakt</h3>
+        <p class="small_margin_bottom">Telefon: +49 (0) 5054 67584</p>
+        <p class="small_margin_bottom">Telefax: +49 (0) 5054 67585</p>
+        <p>E-Mail: info@golf-niedersachsen.de</p>
+
+        <h3 class="entry__content h3">Umsatzsteuer-ID</h3>
+        <p class="small_margin_bottom">Umsatzsteuer-Identifikationsnummer gemäß § 27 a Umsatzsteuergesetz:</p>
+        <p>DE999999999</p>
+
+        <h3 class="entry__content h3">Redaktionell Verantwortlicher</h3>
+        <p class="small_margin_bottom">Wanda Lismus</p>
+        <p class="small_margin_bottom">Widukindweg</p>
+        <p>21357 Barum</p>
+
+        <h3 class="entry__content h3">Verbraucher­streit­beilegung/Universal­schlichtungs­stelle</h3>
+        <p>Wir sind nicht bereit oder verpflichtet, an Streitbeilegungsverfahren vor einer Verbraucherschlichtungsstelle teilzunehmen.</p>
+
+        <h3 class="entry__content h3">Haftung für Inhalte</h3>
+        <p class="small_margin_bottom">Als Diensteanbieter sind wir gemäß § 7 Abs.1 TMG für eigene Inhalte auf diesen Seiten nach den allgemeinen Gesetzen verantwortlich. Nach §§ 8 bis 10 TMG sind wir als Diensteanbieter jedoch nicht verpflichtet, übermittelte oder gespeicherte fremde Informationen zu überwachen oder nach Umständen zu forschen, die auf eine rechtswidrige Tätigkeit hinweisen.</p>
+        <p>Verpflichtungen zur Entfernung oder Sperrung der Nutzung von Informationen nach den allgemeinen Gesetzen bleiben hiervon unberührt. Eine diesbezügliche Haftung ist jedoch erst ab dem Zeitpunkt der Kenntnis einer konkreten Rechtsverletzung möglich. Bei Bekanntwerden von entsprechenden Rechtsverletzungen werden wir diese Inhalte umgehend entfernen.</p>
+
+        <h3 class="entry__content h3">Haftung für Links</h3>
+        <p class="small_margin_bottom">Unser Angebot enthält Links zu externen Websites Dritter, auf deren Inhalte wir keinen Einfluss haben. Deshalb können wir für diese fremden Inhalte auch keine Gewähr übernehmen. Für die Inhalte der verlinkten Seiten ist stets der jeweilige Anbieter oder Betreiber der Seiten verantwortlich. Die verlinkten Seiten wurden zum Zeitpunkt der Verlinkung auf mögliche Rechtsverstöße überprüft. Rechtswidrige Inhalte waren zum Zeitpunkt der Verlinkung nicht erkennbar.</p>
+        <p>Eine permanente inhaltliche Kontrolle der verlinkten Seiten ist jedoch ohne konkrete Anhaltspunkte einer Rechtsverletzung nicht zumutbar. Bei Bekanntwerden von Rechtsverletzungen werden wir derartige Links umgehend entfernen.</p>
+
+        <h3 class="entry__content h3">Urheberrecht</h3>
+        <p class="small_margin_bottom">Die durch die Seitenbetreiber erstellten Inhalte und Werke auf diesen Seiten unterliegen dem deutschen Urheberrecht. Die Vervielfältigung, Bearbeitung, Verbreitung und jede Art der Verwertung außerhalb der Grenzen des Urheberrechtes bedürfen der schriftlichen Zustimmung des jeweiligen Autors bzw. Erstellers. Downloads und Kopien dieser Seite sind nur für den privaten, nicht kommerziellen Gebrauch gestattet.</p>
+        <p>Soweit die Inhalte auf dieser Seite nicht vom Betreiber erstellt wurden, werden die Urheberrechte Dritter beachtet. Insbesondere werden Inhalte Dritter als solche gekennzeichnet. Sollten Sie trotzdem auf eine Urheberrechtsverletzung aufmerksam werden, bitten wir um einen entsprechenden Hinweis. Bei Bekanntwerden von Rechtsverletzungen werden wir derartige Inhalte umgehend entfernen.</p>
+    </div>
+
+    <footer><i class="far fa-copyright"> Copyright 2021</i></footer>
+</body>
